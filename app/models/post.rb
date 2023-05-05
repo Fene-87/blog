53e065e2a0581f93a63dde6378dc300c03a5242c
Fene-87 <fenekayas87@gmail.com>
2023-05-05 12:55:52 +0300

    Add a method that returns the 3 most recent posts for a given user.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+
+    after_save :update_post_counter
+
+    def update_post_counter
+        author.update(posts_counter: author.posts.count)
+      end
 end
