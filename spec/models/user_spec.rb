97d12907b28b73ad802154d28739b011f0d87e3e
Fene-87 <fenekayas87@gmail.com>
2023-05-05 17:13:28 +0300

    Add unit specs for all of your models' methods and validations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    before :all do
+      @user = User.create(name: 'John Doe', photo: 'https://picsum.photos/200/300', bio: 'I am John Doe')
+    end
+
+    it "name can't be blank" do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "photo can't be blank" do
+      @user.photo = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "bio can't be blank" do
+      @user.bio = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "postscounter can't be blank" do
+      @user.postscounter = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'postscounter must be an integer' do
+      @user.postscounter = 'a'
+      expect(@user).to_not be_valid
+    end
+
+    it 'postscounter must be greater than or equal to 0' do
+      @user.postscounter = -1
+      expect(@user).to_not be_valid
+    end
+  end
 end
