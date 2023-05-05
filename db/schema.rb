e995d50bffdf5c30a6d47921ad094cc15a79b711
Fene-87 <fenekayas87@gmail.com>
2023-05-05 13:38:25 +0300

    Fix linters errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_212013) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_102841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "title"
+    t.string "text"
     t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
