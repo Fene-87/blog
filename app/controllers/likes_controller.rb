a889f8b5b9b266ce59b132ab5917a2ef426d1160
Fene-87 <fenekayas87@gmail.com>
2023-05-13 00:26:33 +0300

    Implement Likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,11 @@
 class LikesController < ApplicationController
     def create
+        @post = Post.find(params[:id])
+        @user = current_user
+        @like = Like.new(author: @user, post: @post)
+        @like.save
+
+        flash[:success] = 'Liked'
+        redirect_to user_post_path(@user, @post)
     end
 end
